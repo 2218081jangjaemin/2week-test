0f3f8c7b83986ae53bbb0252d3995ad707e50d85
2218081jangjaemin <144421522+2218081jangjaemin@users.noreply.github.com>
2023-09-08 16:34:09 +0900

    2218054 장재민 퀴즈제출입니다
    퀴즈 제출입니다

---
+++ test.file.py
@@ -0,0 +1,14 @@
+number = input('숫자를입력하세요')
+print(number)
+
+name = input('이름을 입력하세요')
+age = input('나이를 입력하세요')
+print('이른은', name,'나이는', name)
+
+
+a = input('숫자를 입력하세요')
+b = input('숫자를 입력하세요')
+print(int(a)*int(b))
+
+
+name = input('장재민')
